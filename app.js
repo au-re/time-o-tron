e131d96e81052c23f5daff40ccb12755577418e9
Aurélien Hontabat <aurelien.hontabat@fcc.chalmers.se>
2019-01-05 17:10:36 +0100

    add eslintrc, add callSendAPI function

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
-const request = require("request");
+// const request = require("superagent");
 const express = require("express");
-const body_parser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 8080;
-const app = express().use(body_parser.json());
+const app = express().use(bodyParser.json());
 
 // Sets server port and logs message on success
 app.listen(port, () => console.log(`webhook is listening on port: ${port}`));
+
+// async function callSendAPI(senderPsid, response) {
+//   const requestBody = {
+//     recipient: {
+//       id: senderPsid,
+//     },
+//     message: response,
+//   };
+
+//   try {
+//     await request
+//       .post("https://graph.facebook.com/v2.6/me/messages")
+//       .query({ access_token: process.env.PAGE_ACCESS_TOKEN })
+//       .send(requestBody)
+//       .set("X-API-Key", "foobar")
+//       .set("accept", "json");
+//     console.log(`Message sent: ${response} to: ${senderPsid}`);
+//   } catch (error) {
+//     console.error(`Unable to send message: ${error}`);
+//   }
+// }
 
 // Accepts POST requests at /webhook endpoint
 app.post("/webhook", (req, res) => {
   console.log("POST /webhook");
 
   // Parse the request body from the POST
-  let body = req.body;
+  const { body } = req;
 
   // Check the webhook event is from a Page subscription
   if (body.object === "page") {
 
     // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
+    body.entry.forEach((entry) => {
 
       // Get the webhook event. entry.messaging is an array, but
       // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      const webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
 
       // Get the sender PSID
       // let sender_psid = webhook_event.sender.id;
@@ -44,13 +65,12 @@ app.post("/webhook", (req, res) => {
 app.get("/webhook", (req, res) => {
   console.log("GET /webhook");
 
-  /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = "random_cat";
 
   // Parse params from the webhook verification request
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
 
   // Check if a token and mode were sent
   if (mode && token) {
@@ -73,6 +93,6 @@ app.get("/about", (req, res) => {
   console.log("GET /about");
 
   return res.status(200).send({
-    version: "0.0.1"
+    version: "0.0.1",
   });
 });
