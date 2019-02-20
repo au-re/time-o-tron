9dea323ee3b66ce7e3daf829cce79dfb8d9f8a61
Aurélien Hontabat <aurelien.hontabat@fcc.chalmers.se>
2019-02-21 00:54:41 +0100

    part V check 401 issue

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ import { MyBot } from "./bot";
 
 const memoryStorage = new MemoryStorage();
 const conversationState = new ConversationState(memoryStorage);
-const adapter = new BotFrameworkAdapter();
+const adapter = new BotFrameworkAdapter({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD,
+});
 
 const myBot = new MyBot(conversationState);
 
